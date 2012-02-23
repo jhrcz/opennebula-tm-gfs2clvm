d44c185c9f516421680b1424f43cba2fceab3b3e
Jan Horacek (jahor/jhrcz) <jahor@jhr.cz>
2012-02-23 16:37:48 +0100

    spravne poradi definici v ln aby byly definovany kdyz pouzity

--- usr-lib-one-tm_commands/gfs2clvm/tm_ln.sh
+++ usr-lib-one-tm_commands/gfs2clvm/tm_ln.sh
@@ -19,9 +19,6 @@
 SRC=$1
 DST=$2
 
-SRC_PATH=`arg_path $SRC`
-SRC_PATH_BASENAME=$( basename ${SRC_PATH} )
-
 if [ -z "${ONE_LOCATION}" ]; then
     TMCOMMON=/usr/lib/one/mads/tm_common.sh
     TM_COMMANDS_LOCATION=/usr/lib/one/tm_commands/ 
@@ -34,6 +31,9 @@ fi
 
 . $TMCOMMON
 . $LVMRC
+
+SRC_PATH=`arg_path $SRC`
+SRC_PATH_BASENAME=$( basename ${SRC_PATH} )
 
 log "Link $SRC_PATH (non shared dir, will clone)"
 #exec_and_log "ln -s $SRC_PATH $DST_PATH"
